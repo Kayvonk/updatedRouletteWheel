b391456051090bb2bcfd362c19ff4ab0bae74954
Kayvon Kazemini <kayvonk@gmail.com>
2024-11-20 00:58:07 -0700

    updated wheel functionality

--- src/Wheel.js
+++ src/Wheel.js
@@ -42,29 +42,28 @@ const Wheel = () => {
     14,
     2,
   ];
-  const reds = [
-    1, 3, 5, 7, 9, 12, 14, 16, 18, 21, 23, 25, 27, 28, 30, 32, 34, 36,
-  ];
-  const blacks = [
-    2, 4, 6, 8, 10, 11, 13, 15, 17, 19, 20, 22, 24, 26, 29, 31, 33, 35,
-  ];
 
   return (
     <div className="wheelContainer">
       <div className="pointer"></div>
       <div className="wheel">
         {numbersArray.map((number, index) => {
-          // Notice that there is a new const for the angle.
-          // This is because we will be multipying the angle to get the skew value.
-          // The skew is going to change dynamically with the angle.
           const angle = 360 / numbersArray.length;
           const rotation = angle * index;
           const skew = angle * -8.5;
+
           return (
             <div
-              className="segment"
-              style={{ transform: `rotate(${rotation}deg) skewY(${skew}deg)` }}
+              className="segmentContainer"
+              key={index}
+              style={{ transform: `rotate(${rotation}deg)` }}
             >
+              <div
+                className="segment"
+                style={{
+                  transform: `skewY(${skew}deg)`,
+                }}
+              ></div>
               <div className="number">{number}</div>
             </div>
           );
